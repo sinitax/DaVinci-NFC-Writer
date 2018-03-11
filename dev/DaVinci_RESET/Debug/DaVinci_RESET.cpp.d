51b54958b1df4b46cf684d3b5b9cc4e84cc847c7
Louis Burda <quent.burda@gmail.com>
2018-03-11 23:03:08 +0100

    Fixing endianness

---
+++ dev/DaVinci_RESET/Debug/DaVinci_RESET.cpp.d
@@ -0,0 +1,19 @@
+C:\Users\Sinitax\AppData\Local\Temp\VMBuilds\DaVinci_RESET\mini_atmega168\Debug\DaVinci_RESET.cpp.o: \
+ C:\Users\Sinitax\AppData\Local\Temp\VMBuilds\DaVinci_RESET\mini_atmega168\Debug\DaVinci_RESET.cpp \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/binary.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WCharacter.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WString.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/HardwareSerial.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Stream.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Print.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Printable.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/USBAPI.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\variants\eightanaloginputs/pins_arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\variants\eightanaloginputs/../standard/pins_arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\libraries\SPI\src/SPI.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\libraries\rfid-master\src/MFRC522.h \
+ C:\Program\ Files\ (x86)\Arduino\libraries\rfid-master\src/require_cpp11.h \
+ C:\Program\ Files\ (x86)\Arduino\libraries\rfid-master\src/deprecated.h
--- dev/DaVinci_RESET/__vm/.DaVinci_RESET.vsarduino.h
+++ dev/DaVinci_RESET/__vm/.DaVinci_RESET.vsarduino.h
@@ -5,21 +5,18 @@
 			all non-arduino files created by visual micro and all visual studio project or solution files can be freely deleted and are not required to compile a sketch (do not delete your own code!).
 			note: debugger breakpoints are stored in '.sln' or '.asln' files, knowledge of last uploaded breakpoints is stored in the upload.vmps.xml file. Both files are required to continue a previous debug session without needing to compile and upload again
 	
-	Hardware: Arduino Yún, Platform=avr, Package=arduino
+	Hardware: Arduino Mini w/ ATmega168, Platform=avr, Package=arduino
 */
 
 #if defined(_VMICRO_INTELLISENSE)
 
 #ifndef _VSARDUINO_H_
 #define _VSARDUINO_H_
-#define __AVR_ATmega32u4__
-#define __AVR_ATmega32U4__
+#define __AVR_ATmega168__
 #define F_CPU 16000000L
 #define ARDUINO 10804
-#define ARDUINO_AVR_YUN
+#define ARDUINO_AVR_MINI
 #define ARDUINO_ARCH_AVR
-#define USB_VID 0x2341
-#define USB_PID 0x8041
 #define __cplusplus 201103L
 #define __AVR__
 #define __inline__
